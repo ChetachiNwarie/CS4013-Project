2ea5cb1d9fc5e81e86669f408626fcfe201cfc6f
samoma451 <74059412+samoma451@users.noreply.github.com>
2020-12-05 16:55:58 +0000

    Add files via upload

--- SamCode/DepartmentManagementMenu.java
+++ SamCode/DepartmentManagementMenu.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
 
 /**
@@ -69,14 +70,18 @@ public class DepartmentManagementMenu {
         String routekey = in.nextLine().toUpperCase();
         ArrayList<Property> areaProperties = areaRecords(routekey);
         
+        return statistics(areaProperties);        
+    }
+    
+    private String statistics(ArrayList<Property> props){    
         String stats = "";
         double totalVal = 0;
         int numRecords=0;
         int numPaidRecords=0;
         
-        for(int i = 0; i<areaProperties.size(); i++){
-            totalVal+=areaProperties.get(i).getMarketValue();
-            ArrayList<PaymentRecord> payRecords = areaProperties.get(i).getAllRecords();
+        for(int i = 0; i<props.size(); i++){
+            totalVal+=props.get(i).getMarketValue();
+            ArrayList<PaymentRecord> payRecords = props.get(i).getAllRecords();
             numRecords += payRecords.size();
             for(int j =0; j < payRecords.size(); j++){
                 if(payRecords.get(i).getWasPaid()){
@@ -87,10 +92,152 @@ public class DepartmentManagementMenu {
         
         
         return String.format("Total tax paid : %.2f\nAverage tax paid: %.2f\nNumber of property taxes paid : %d\nPercentage of taxes paid : %.2f\n",
-                totalVal, (totalVal/areaProperties.size()), numRecords, (numPaidRecords/numRecords));
+                totalVal, (totalVal/props.size()), numRecords, (numPaidRecords/numRecords));
     }
     
     public String investigate(){
-    
+        String choice = "";
+        
+        do{
+        System.out.println("Would you like to change the fixed cost? Y/N (Default=100)");
+        choice = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double newCost = -1;
+            do{
+                System.out.println("Enter the new fixed cost: ");
+                newCost = in.nextInt();
+            }while(newCost<0);
+            Property.setFixedCost(newCost);
+        }
+        
+        do{
+        System.out.println("Would you like to change the brackets of the values for market value tax? Y/N (Default=150000, 400000, 650000)");
+        String choice2 = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double firstBracket = -1;
+            do{
+                System.out.println("Enter the new first bracket: ");
+                firstBracket = in.nextDouble();
+            }while(firstBracket<0);
+            
+            double secondBracket = -1;
+            do{
+                System.out.println("Enter the new second bracket: ");
+                secondBracket = in.nextDouble();
+            }while(secondBracket<firstBracket);
+            
+            double thirdBracket = -1;
+            do{
+                System.out.println("Enter the new third bracket: ");
+                thirdBracket = in.nextDouble();
+            }while(thirdBracket<secondBracket);
+            
+            double[] newBrackets = {0, firstBracket, secondBracket, thirdBracket};
+            Property.setValueBrackets(newBrackets);
+                       
+        }
+        
+        do{
+        System.out.println("Would you like to change the rates for market value tax? Y/N (Default=0, 0.0001, 0.0002, 0.0004)");
+        choice = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double firstRate = -1;
+            do{
+                System.out.println("Enter the new first rate: ");
+                firstRate = in.nextDouble();
+            }while(firstRate<0 || firstRate>1);
+            
+            double secondRate = -1;
+            do{
+                System.out.println("Enter the new second rate: ");
+                secondRate = in.nextDouble();
+            }while(secondRate<firstRate || secondRate>1);
+            
+            double thirdRate = -1;
+            do{
+                System.out.println("Enter the new third rate: ");
+                thirdRate = in.nextDouble();
+            }while(thirdRate<secondRate || secondRate>1);
+            
+            double fourthRate = -1;
+            do{
+                System.out.println("Enter the new fourth rate: ");
+                fourthRate = in.nextDouble();
+            }while(fourthRate<thirdRate || secondRate>1);
+            
+            double[] newRates = {firstRate, secondRate, thirdRate, fourthRate};
+            Property.setValueBracketRates(newRates);                       
+        }
+        
+        do{
+        System.out.println("Would you like to change the rates for location category? Y/N (Default=25, 50, 60, 80, 100)");
+        choice = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double firstRate = -1;
+            do{
+                System.out.println("Enter the new first rate: ");
+                firstRate = in.nextDouble();
+            }while(firstRate<0);
+            
+            double secondRate = -1;
+            do{
+                System.out.println("Enter the new second rate: ");
+                secondRate = in.nextDouble();
+            }while(secondRate<firstRate);
+            
+            double thirdRate = -1;
+            do{
+                System.out.println("Enter the new third rate: ");
+                thirdRate = in.nextDouble();
+            }while(thirdRate<secondRate);
+            
+            double fourthRate = -1;
+            do{
+                System.out.println("Enter the new fourth rate: ");
+                fourthRate = in.nextDouble();
+            }while(fourthRate<thirdRate);
+            
+            double fifthRate = -1;
+            do{
+                System.out.println("Enter the new fifth rate: ");
+                fifthRate = in.nextDouble();
+            }while(fifthRate<fourthRate);
+            
+            double[] newRates = {firstRate, secondRate, thirdRate, fourthRate, fifthRate};
+            Property.setLocationCatRates(newRates);
+        }
+        
+        do{
+        System.out.println("Would you like to change the charge for the property "
+                + "not being the principal private residence of the owner? Y/N (Default=100)");
+        choice = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double newCharge = -1;
+            do{
+                System.out.println("Enter the new charge: ");
+                newCharge = in.nextDouble();
+            }while(newCharge<0);
+            Property.setPrincipalPrivateRate(newCharge);
+        }
+        
+        do{
+        System.out.println("Would you like to change the penalty for previous unpaid years? Y/N (Default=0.07)");
+        choice = in.nextLine().toUpperCase();
+    }while(!choice.equals("Y") && !choice.equals("N"));
+        if(choice.equals("Y")){
+            double newPenalty = -1;
+            do{
+                System.out.println("Enter the new penalty: ");
+                newPenalty = in.nextDouble();
+            }while(newPenalty<0 || newPenalty >1);
+            Property.setUnpaidPenalty(newPenalty);
+        }        
+        
+        return statistics(allProperties);
     }
 }
--- SamCode/Property.java
+++ SamCode/Property.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -15,13 +16,13 @@ public class Property {
     private boolean principalPrivateResidence;
     private ArrayList<PaymentRecord> paymentRecords;
     //constants to calculate tax from here on
-    private double fixedCost = 100;
-    private double[] valueBrackets = {0, 150000, 400000, 650000};
-    private double[] valueBracketRates = {0, 0.0001, 0.0002, 0.0004};
-    private double[] locationCatRates = {100, 80, 60, 50, 25};
-    private double principalPrivateRate = 100;
-    private double unpaidPenalty = 0.07;
-    private int currentYear = LocalDate.now().getYear();
+    private static double fixedCost = 100;
+    private static double[] valueBrackets = {0, 150000, 400000, 650000};
+    private static double[] valueBracketRates = {0, 0.0001, 0.0002, 0.0004};
+    private static double[] locationCatRates = {25, 50, 60, 80, 100};
+    private static double principalPrivateRate = 100;
+    private static double unpaidPenalty = 0.07;
+    private static int currentYear = LocalDate.now().getYear();
 
     public Property(String owner, String address, String eircode, double marketValue, String locationCategory, boolean principalPrivateResidence){
         this.owner=owner;
@@ -62,21 +63,22 @@ public class Property {
         //charge based on location
         switch(locationCategory){
 
-            case "city":
-                taxDue+=100;
-                break;
-            case "large town":
-                taxDue+=80;
-                break;
-            case "small town":
-                taxDue+=60;
+            case "countryside":
+                taxDue+=locationCatRates[0];
                 break;
             case "village":
-                taxDue+=50;
+                taxDue+=locationCatRates[1];
                 break;
-            case "countryside":
-                taxDue+=25;
+            case "small town":
+                taxDue+=locationCatRates[2];
                 break;
+            case "large town":
+                taxDue+=locationCatRates[3];
+                break;
+            case "city":
+                taxDue+=locationCatRates[4];
+                break;                   
+            
         }
 
         //charge if not the principal private residence
@@ -94,8 +96,10 @@ public class Property {
     private ArrayList<PaymentRecord> getOverdueRecords(){
         ArrayList<PaymentRecord> overdueRecords = new ArrayList<>();
         for(int i = 0; i<paymentRecords.size(); i++){
+            if(paymentRecords.get(i).getYear()!=currentYear){
             if(!paymentRecords.get(i).getWasPaid()){
                 overdueRecords.add(paymentRecords.get(i));
+            }
             }
         }
         return overdueRecords;
@@ -106,6 +110,29 @@ public class Property {
         for(int i = 0; i<overdueRecords.size();i++){
 
         }
+    }
+    
+    //used for investigate method in DepartmentManagementMenu
+    public static void setFixedCost(double newCost){
+        fixedCost = newCost;
+    }
+    public static void setValueBrackets(double[] newBrackets){
+        Arrays.sort(newBrackets);
+        valueBrackets=newBrackets;
+    }
+    public static void setValueBracketRates(double[] newRates){
+        Arrays.sort(newRates);
+        valueBracketRates=newRates;
+    }
+    public static void setLocationCatRates(double[] newRates){
+        Arrays.sort(newRates);
+        locationCatRates=newRates;
+    }
+    public static void setPrincipalPrivateRate(double rate){
+        principalPrivateRate=rate;
+    }
+    public static void setUnpaidPenalty(double newPenalty){
+        unpaidPenalty=newPenalty;
     }
     
     //Chetachi's code from here down
