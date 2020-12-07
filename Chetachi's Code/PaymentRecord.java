a9d237f1615ed50476df70df3d3520c90df16297
ChetachiNwarie <74025347+ChetachiNwarie@users.noreply.github.com>
2020-12-07 17:30:45 +0000

    Add files via upload

---
+++ Chetachi's Code/PaymentRecord.java
@@ -0,0 +1,109 @@
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Samuel O'Mahony (19236719)
+ */
+//Sam's payment record class
+public class PaymentRecord
+{
+
+    private int year;
+    private boolean wasPaid;
+    private double amount;
+
+    public PaymentRecord(int year, boolean wasPaid, double amount)
+    {
+        this.year = year;
+        this.wasPaid = wasPaid;
+        this.amount = amount;
+    }
+
+    public boolean isWasPaid()
+    {
+        return wasPaid;
+    }
+
+    public void setWasPaid(boolean wasPaid)
+    {
+        this.wasPaid = wasPaid;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public boolean getWasPaid()
+    {
+        return wasPaid;
+    }
+    
+    
+   // Chetachi's code from here
+    public void replaceLine(String filename, String change, int column)
+    {
+        try
+        {
+            BufferedReader file = new BufferedReader(new FileReader(filename));
+            StringBuffer inputBuffer = new StringBuffer();
+            String line;
+
+            while ((line = file.readLine()) != null)
+            {
+                if (line.equals(toString()))
+                {
+                    String[] split = line.split(",");
+                    split[column] = change;
+
+                    String a = Arrays.toString(split);
+                    System.out.println(a);
+                    String[] split2 = a.split("(\\[)|(\\])");
+                    line = split2[1];
+                }
+
+                inputBuffer.append(line);
+                inputBuffer.append('\n');
+            }
+            file.close();
+            FileOutputStream fileOut = new FileOutputStream(filename, false);
+            fileOut.write(inputBuffer.toString().getBytes());
+            fileOut.close();
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Problem reading file.");
+        }
+    }
+
+    @Override
+    public String toString()
+    {
+        String p = "";
+        if (getWasPaid())
+        {
+            p = "yes";
+        }
+        else
+        {
+            p = "no";
+        }
+        String b = String.format("%d,%.2f,%s", getYear(), getAmount(), p);
+        return b;
+    }
+}
