257584cc87783ed1bdbbf6d1f2f3848e07aedca5
ChetachiNwarie <74025347+ChetachiNwarie@users.noreply.github.com>
2020-12-07 17:33:00 +0000

    Update PropertyMain.java

--- Chetachi's Code/PropertyMain.java
+++ Chetachi's Code/PropertyMain.java
@@ -11,16 +11,31 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author cnwar
+ * @author Chetachi Nwarie (19244355)
  */
 public class PropertyMain
 {
     public static void main (String[]args){
-        try
-        {
-            PropertyManagement prop = new PropertyManagement();
-            PropertyChargeManagementSystem p = new PropertyChargeManagementSystem();
-            p.run(prop);
+          try{
+        Property sam = new Property("Sam", "4 house", "V34He33", 2132322, "Large Town", true);
+        sam.addToPaymentFile(new PaymentRecord(2010, false, 200));
+        sam.addToPaymentFile(new PaymentRecord(2011, false, 200));
+        sam.addToPaymentFile(new PaymentRecord(2012, false, 200));
+        sam.addToPaymentFile(new PaymentRecord(2013, false, 200));
+        sam.addToPaymentFile(new PaymentRecord(2014, false, 200));
+        sam.addToPaymentFile(new PaymentRecord(2015, false, 200));
+        Property aoife = new Property("Aoife", "34 Lenov", "V34Hr33", 21322422, "Small Town", false);
+        aoife.addToPaymentFile(new PaymentRecord(2010, false, 200));
+        aoife.addToPaymentFile(new PaymentRecord(2011, false, 200));
+        aoife.addToPaymentFile(new PaymentRecord(2012, false, 200));
+        aoife.addToPaymentFile(new PaymentRecord(2013, false, 200));
+        aoife.addToPaymentFile(new PaymentRecord(2014, false, 200));
+        aoife.addToPaymentFile(new PaymentRecord(2015, false, 200));
+        
+      
+        PropertyManagement prop = new PropertyManagement();
+        PropertyChargeManagementSystem p = new PropertyChargeManagementSystem();
+        p.run(prop);
         }
         catch (IOException ex)
         {
