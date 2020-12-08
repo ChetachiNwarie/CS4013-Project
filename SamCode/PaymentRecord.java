131032b3e0ae811d185b959f05547feb1ba829e6
samoma451 <74059412+samoma451@users.noreply.github.com>
2020-12-08 20:38:24 +0000

    Add files via upload

--- SamCode/PaymentRecord.java
+++ SamCode/PaymentRecord.java
@@ -1,5 +1,6 @@
 /**
- *
+ * The PaymentRecord class represents the record of a year of property tax.
+ * 
  * @author Samuel O'Mahony (19236719)
  */
 public class PaymentRecord {
@@ -8,17 +9,45 @@ public class PaymentRecord {
     private boolean wasPaid;
     private double amount;
     
+    /**
+     * Constructor for objects of class PaymentRecord
+     * @param year The year associated with the record
+     * @param wasPaid Has the tax for this year been paid? True if it has, false if not
+     * @param amount The amount paid that year
+     */
     public PaymentRecord(int year, boolean wasPaid, double amount){
         this.year=year;
         this.wasPaid=wasPaid;
         this.amount=amount;
     }
     
+    /**
+     * Returns the year associated with this record
+     * @return The year associated with this record
+     */
     public int getYear(){
         return year;
     }
-    
+    /**
+     * Returns true if the tax for this year was paid, false if it was not.
+     * @return True if the tax for this year was paid, false if it was not.
+     */
     public boolean getWasPaid(){
         return wasPaid;
+    }
+    
+    /**
+     * Sets this PaymentRecord as paid(true) or unpaid(false)
+     * @param wasPaid True if setting as paid, false if setting as unpaid
+     */
+    public void setWasPaid(boolean wasPaid){
+        this.wasPaid=wasPaid;
+    }
+    /**
+     * Sets the amount paid for the year corresponding to this PaymentRecord
+     * @param amount The amount of tax paid for this year
+     */
+    public void setAmount(double amount){
+        this.amount=amount;
     }
 }
--- SamCode/Property.java
+++ SamCode/Property.java
@@ -3,11 +3,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- *
+ * This class represents a Property in a system that calculates and records
+ * the property tax on properties. 
+ * 
  * @author Samuel O'Mahony (19236719)
  */
 public class Property {
 
+    //attributes a property has
     private String owner;
     private String address;
     private String eircode;
@@ -15,7 +18,8 @@ public class Property {
     private String locationCategory;
     private boolean principalPrivateResidence;
     private ArrayList<PaymentRecord> paymentRecords;
-    //constants to calculate tax from here on
+    
+    //constants to calculate tax
     private static double fixedCost = 100;
     private static double[] valueBrackets = {0, 150000, 400000, 650000};
     private static double[] valueBracketRates = {0, 0.0001, 0.0002, 0.0004};
@@ -24,6 +28,15 @@ public class Property {
     private static double unpaidPenalty = 0.07;
     private static int currentYear = LocalDate.now().getYear();
 
+    /**
+     * Constructor for objects of class Property 
+     * @param owner The owner of the property
+     * @param address The address of the property
+     * @param eircode The eircode of the property
+     * @param marketValue The market value of the property
+     * @param locationCategory The location category e.g. city, small town, countryside
+     * @param principalPrivateResidence Is the property the principal private residence of the owner? If yes then true, if no then false
+     */
     public Property(String owner, String address, String eircode, double marketValue, String locationCategory, boolean principalPrivateResidence){
         this.owner=owner;
         this.address=address;
@@ -33,7 +46,13 @@ public class Property {
         this.principalPrivateResidence=principalPrivateResidence;
     }
 
-
+    /**
+     * Returns the PaymentRecord object corresponding to a given year for this property or 
+     * null if no such record exists.
+     * 
+     * @param year The year of the record wanted
+     * @return The PaymentRecord for the year given by the user or null if no such record exists. 
+     */
     public PaymentRecord getRecord(int year){
         for(int i = 0; i<paymentRecords.size(); i++){
             if(paymentRecords.get(i).getYear()==year){
@@ -44,11 +63,16 @@ public class Property {
         return null;
     }
     
+    /**
+     * Returns an ArrayList containing all the PaymentRecords for this property
+     * 
+     * @return An ArrayList containing all the PaymentRecords for this property
+     */
     public ArrayList<PaymentRecord> getAllRecords(){
         return paymentRecords;
     }
-
-    public double taxDue(){
+    
+    private double taxDueThisYear(){
         //fixed rate
         double taxDue = fixedCost;
 
@@ -85,14 +109,35 @@ public class Property {
         if(!principalPrivateResidence){
             taxDue+=100;
         }
-
-        //overdue penalty
-        int yearsOverdue = this.getOverdueRecords().size();
-        taxDue=taxDue*Math.pow(1+unpaidPenalty, yearsOverdue);
-
+        
         return taxDue;
     }
 
+    /**
+     *  Calculates and returns the tax due on this property based on the fixed rate,
+     * market value, location, whether or not it is the principal private residence
+     * of the owner, and how many previous years are unpaid plus the penalty for 
+     * a year remaining unpaid.
+     * @return The tax due on this property
+     */
+    public double taxDue(){    
+        //overdue penalty
+        double taxBeforePenalty=taxDueThisYear();
+        double taxDue=taxBeforePenalty;
+        ArrayList<PaymentRecord> yearsOverdue = this.getOverdueRecords();
+        for(int i = 0; i<yearsOverdue.size(); i++){
+            int pow = currentYear-yearsOverdue.get(i).getYear();//pow is the no. of years for which a penalty applies
+            taxDue+=taxBeforePenalty*Math.pow(1+unpaidPenalty, pow);
+        }        
+        
+        return taxDue;
+    }
+
+    /**
+     * Checks each PaymentRecord associated with this property and returns an ArrayList
+     * of all overdue/unpaid records.
+     * @return An ArrayList of the unpaid records associated with this property
+     */
     private ArrayList<PaymentRecord> getOverdueRecords(){
         ArrayList<PaymentRecord> overdueRecords = new ArrayList<>();
         for(int i = 0; i<paymentRecords.size(); i++){
@@ -105,32 +150,65 @@ public class Property {
         return overdueRecords;
     }
 
+    /**
+     * Sets the PaymentRecord's wasPaid boolean to true for each year and sets
+     * the amount paid for that year.
+     */
     public void payTax(){
         ArrayList<PaymentRecord> overdueRecords = this.getOverdueRecords();
+              
         for(int i = 0; i<overdueRecords.size();i++){
-
+            overdueRecords.get(i).setWasPaid(true);
+            int pow = currentYear-overdueRecords.get(i).getYear();
+            double amount = taxDueThisYear()*Math.pow(1+unpaidPenalty, pow);
+            overdueRecords.get(i).setAmount(amount);
         }
     }
     
-    //used for investigate method in DepartmentManagementMenu
+    /**
+     * Sets the fixed cost
+     * @param newCost The new fixed cost
+     */
     public static void setFixedCost(double newCost){
         fixedCost = newCost;
     }
+    /**
+     * Sets the brackets of market value that decide which category of market value
+     * a property should be charged for.
+     * @param newBrackets An array with the new market value brackets
+     */
     public static void setValueBrackets(double[] newBrackets){
         Arrays.sort(newBrackets);
         valueBrackets=newBrackets;
     }
+    /**
+     * Sets the rates charged for properties in each market value bracket
+     * @param newRates An array containing the new rates for each market value bracket
+     */
     public static void setValueBracketRates(double[] newRates){
         Arrays.sort(newRates);
         valueBracketRates=newRates;
     }
+    /**
+     * Sets the rates charged for properties in each location category
+     * @param newRates An array of the new rates for each location category
+     */
     public static void setLocationCatRates(double[] newRates){
         Arrays.sort(newRates);
         locationCatRates=newRates;
     }
+    /**
+     * Sets the cost of the charge in place if a property is not the principal private
+     * residence of the owner
+     * @param rate The new cost of the charge
+     */
     public static void setPrincipalPrivateRate(double rate){
         principalPrivateRate=rate;
     }
+    /**
+     * Sets the penalty applied for each year tax is unpaid
+     * @param newPenalty The new penalty to be applied
+     */
     public static void setUnpaidPenalty(double newPenalty){
         unpaidPenalty=newPenalty;
     }
